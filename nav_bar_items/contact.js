a17c63a4eb6d88c201acdf8bca4e2a7637607159
Vijayarajamayuri <avrmayuri74@gmail.com>
2020-02-23 20:12:10 -0600

    contact form connecting to firebase

---
+++ nav_bar_items/contact.js
@@ -0,0 +1,12 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyCX-oPHwlu_uEhrNYTlU8ZcFDPaTKNJKOQ",
+    authDomain: "gdp-app-dbb67.firebaseapp.com",
+    databaseURL: "https://gdp-app-dbb67.firebaseio.com/",
+    projectId: "gdp-app-dbb67",
+    storageBucket: "gdp-app-dbb67.appspot.com",
+    messagingSenderId: "483028094395",
+    appId: "1:483028094395:web:05b54aa719d10231ef089e",
+    measurementId: "G-67DHJ32WL0"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
